0bac7e7a64f2ac75777c0d28e2e46a2aec2085a0
Yura Kochut <kochutyurawork@gmail.com>
2020-04-03 13:14:12 +0300

    add ( contact-information | for desctop )

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import { UserWindowService } from './shared/services/user-window.service';
 import { NavbarService } from './shared/services/navbar.service';
 import { HomeService } from './shared/services/pages/home.service';
+import { ContactInformationService } from './shared/services/pages/contact-information.service';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,8 @@ export class AppComponent implements OnInit {
   constructor(
     private windowS: UserWindowService,
     private navbarS: NavbarService,
-    private homeS: HomeService
+    private homeS: HomeService,
+    private contactInfoS: ContactInformationService
   ) { }
   
   @HostListener('window:resize', ['$event']) onResize(event) {
@@ -21,12 +23,14 @@ export class AppComponent implements OnInit {
     this.windowS.height = event.target.innerHeight;
     this.homeS.homeSizeBlock();
     this.navbarS.resetToggle();
+    this.contactInfoS.scaleBlockContactInfo();
   }
 
   ngOnInit(): void {
     this.windowS.width = window.innerWidth
     this.windowS.height = window.innerHeight;
     this.homeS.homeSizeBlock();
+    this.contactInfoS.scaleBlockContactInfo();
     this.navbarS.resetToggle();
   }
 
