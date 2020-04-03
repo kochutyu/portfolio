8f853040225c7b3535ae7bf60e5d5e5cbf16e779
Yura Kochut <kochutyurawork@gmail.com>
2020-04-03 18:54:33 +0300

    update ( slider | small corrections )

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -19,43 +19,24 @@ export class SliderComponent implements OnInit {
 
   checkNextSlide: boolean = false;
   OnNextSlide(): void {
-    let clear = setTimeout(() => {
-      
-      if (this.nextSlide.nativeElement.childNodes[0]) {
-        if (this.iSelect === 0 || this.iSelect < this.img.length - 1) this.iSelect += 1;
-        else this.iSelect = 0;
+    let clear = setInterval(() => {
+      if (!this.checkNextSlide) {
+        if (this.nextSlide.nativeElement.childNodes[0]) {
+          if (this.iSelect === 0 || this.iSelect < this.img.length - 1) this.iSelect += 1;
+          else this.iSelect = 0;
+        } else {
+          if (this.iSelect === 0 || this.iSelect < this.img.length - 1) this.iSelect += 1;
+          else this.iSelect = 0;
+        }
       } else {
-        if (this.iSelect === 0 || this.iSelect < this.img.length - 1) this.iSelect += 1;
-        else this.iSelect = 0;
+        clearInterval(clear);
+        this.checkNextSlide = false;
+        this.OnNextSlide();
       }
-        // this.checkNextSlide = false;
-        // clearInterval(clear);
-      this.OnNextSlide();
-      let a = 0;
-      a += 1;
-      console.log(a);
-      
-
-
-      // if (!this.checkNextSlide) {
-      //   if (this.nextSlide.nativeElement.childNodes[0]) {
-      //     if (this.iSelect === 0 || this.iSelect < this.img.length - 1) this.iSelect += 1;
-      //     else this.iSelect = 0;
-      //   } else {
-      //     if (this.iSelect === 0 || this.iSelect < this.img.length - 1) this.iSelect += 1;
-      //     else this.iSelect = 0;
-      //   }
-      //   console.log(1);
-      //   this.checkNextSlide = false;
-      //   clearInterval(clear);
-      //   this.OnNextSlide();
-      // } else {
-      //   console.log(1);
-        
-      // }
-    }, 100);
+    }, 5000);
   }
-  thisItem(item): void {
+
+  thisItemSelected(item): void {
     this.iSelect = item;
     this.checkNextSlide = true;
   }
