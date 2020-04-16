e0d3d27d8dcb00c565a5ba237ee2c3aac5a74894
Yura Kochut <kochutyurawork@gmail.com>
2020-04-16 14:11:41 +0300

    update ( about | get info about all tags )

--- src/app/shared/services/pages/about.service.ts
+++ src/app/shared/services/pages/about.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { IAbout, Work, IProgress, ITags } from '../../interface/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AboutService {
-  description: string = 'Значимость этих проблем настолько очевидна, что дальнейшее развитие различных форм деятельности в значительной степени обуславливает создание дальнейших направлений развития. Товарищи! консультация с широким активом играет важную роль в формировании форм развития. Повседневная практика показывает, что укрепление и развитие структуры способствует подготовки и реализации форм развития. Идейные соображения высшего порядка, а также сложившаяся структура организации играет важную роль в формировании соответствующий условий активизации. Задача организации, в особенности же новая модель организационной деятельности позволяет оценить значение существенных финансовых и административных условий. Повседневная практика показывает, что начало повседневной работы по формированию позиции способствует подготовки и реализации форм развития.';
+  about: IAbout = {
+    id: null,
+    descriptionAbout: null,
+    sliderImgURL: null
+  }
+
+  works: Work[];
+
+  progressWorksNotFiltered: ITags[];
+  progressWorksFiltered: IProgress[] = [];
   constructor() { }
+
+
+  getProgressPercent(): void {
+    const objectStr: any[] = this.progressWorksNotFiltered.map(work => {
+      return { tag: work.tags.join(',') } // get arrObj
+    });
+    
+    let tagStr: string = '';
+
+    objectStr.forEach(tag => {
+      tagStr = `${tagStr},${tag.tag}` // get fall str with all tags
+    })
+
+    let allTags: string[] = tagStr.match(/([a-zA-Z]+)/g);
+
+    allTags.forEach(items => {
+      const tag = allTags.filter(item => item === items); // get count same tags
+
+      const IProgress: IProgress = {
+        tag: tag[0],
+        percent: (tag.length * 100 / this.works.length).toString()
+      }
+
+      if (items === tag[0]) { // if exist tag in allArr
+        this.progressWorksFiltered.push(IProgress);
+      }
+
+      allTags = allTags.filter((item, i) => item !== items); // delete the same tags
+    });
+
+  }
 }
